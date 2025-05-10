b8060ba2fdc4890bdb52459a77a287064890f0e6
Heist1337 <mihail.skuratovsky@yandex.ru>
2025-05-10 19:28:43 +0300

    Добавлен новый модуль admin_panel в настройки и маршруты приложения. Обновлены шаблоны для улучшения навигации, включая активные ссылки для администраторов. Улучшен интерфейс навигационной панели.

--- Tracker33/urls.py
+++ Tracker33/urls.py
@@ -42,6 +42,7 @@ class CustomLoginView(auth_views.LoginView):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin-panel/', include('admin_panel.urls', namespace='admin_panel')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
