4db6448aefe6a2f5995c3d05222efa7d6ff5e66e
Михаил Хейст <heist.mind@proton.me>
2025-06-05 06:15:53 +0300

    Обновлены настройки маршрутизации для обслуживания статических файлов в режиме DEBUG и вне его. Удалены устаревшие лог-файлы и бинарные файлы, улучшена читаемость логов производительности.

--- Tracker33/urls.py
+++ Tracker33/urls.py
@@ -57,6 +57,6 @@ urlpatterns = [
     ])),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Обслуживание статики - работает и в DEBUG=True и в DEBUG=False
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
